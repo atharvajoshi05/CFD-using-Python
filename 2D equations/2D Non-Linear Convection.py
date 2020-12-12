5857bd41c919a9619644f0165f2cb8da23b413c2
atharvajoshi05 <56198727+atharvajoshi05@users.noreply.github.com>
2020-12-12 11:26:44 +0400

    2D equations

---
+++ 2D equations/2D Non-Linear Convection.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 12 09:45:54 2020
+
+@author: Atharva
+"""
+
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import pyplot, cm
+import numpy
+
+nx = 101
+ny = 101
+nt = 80
+c = 1
+dx = 2 / (nx - 1)
+dy = 2 / (ny - 1)
+sigma = .2
+dt = sigma * dx
+
+x = numpy.linspace(0, 2, nx)
+y = numpy.linspace(0, 2, ny)
+
+u = numpy.ones((ny, nx)) #create a 1xn vector of 1's
+v = numpy.ones((ny, nx))
+un = numpy.ones((ny, nx))
+vn = numpy.ones((ny, nx))
+
+#Assigning ICs
+#set hat function I.C. : u(.5<=x<=1 && .5<=y<=1 ) is 2
+u[int(.5 / dy):int(1 / dy + 1), int(.5 / dx):int(1 / dx + 1)] = 2
+
+##set hat function I.C. : v(.5<=x<=1 && .5<=y<=1 ) is 2
+v[int(.5 / dy):int(1 / dy + 1), int(.5 / dx):int(1 / dx + 1)] = 2
+
+# plotting the ICs
+fig = pyplot.figure(figsize=(11, 7), dpi=100)
+ax = fig.gca(projection='3d')
+X, Y = numpy.meshgrid(x, y)
+
+ax.plot_surface(X, Y, u, cmap=cm.viridis, rstride=2, cstride=2)
+ax.set_xlabel('$x$')
+ax.set_ylabel('$y$');
+
+# Assigning BCs
+
+for n in range(nt + 1): #loop across number of time steps
+    un = u.copy()
+    vn = v.copy()
+    u[1:, 1:] = (un[1:, 1:] - 
+                 (un[1:, 1:] * c * dt / dx * (un[1:, 1:] - un[1:, :-1])) -
+                  vn[1:, 1:] * c * dt / dy * (un[1:, 1:] - un[:-1, 1:]))
+    v[1:, 1:] = (vn[1:, 1:] -
+                 (un[1:, 1:] * c * dt / dx * (vn[1:, 1:] - vn[1:, :-1])) -
+                 vn[1:, 1:] * c * dt / dy * (vn[1:, 1:] - vn[:-1, 1:]))
+    
+    u[0, :] = 1
+    u[-1, :] = 1
+    u[:, 0] = 1
+    u[:, -1] = 1
+    
+    v[0, :] = 1
+    v[-1, :] = 1
+    v[:, 0] = 1
+    v[:, -1] = 1
+    
+# For plotting 'u' 
+fig = pyplot.figure(figsize=(11, 7), dpi=100)
+ax = fig.gca(projection='3d')
+X, Y = numpy.meshgrid(x, y)
+
+ax.plot_surface(X, Y, u, cmap=cm.viridis, rstride=2, cstride=2)
+ax.set_xlabel('$x$')
+ax.set_ylabel('$y$');
+
+# For plotting 'v'
+fig = pyplot.figure(figsize=(11, 7), dpi=100)
+ax = fig.gca(projection='3d')
+X, Y = numpy.meshgrid(x, y)
+ax.plot_surface(X, Y, v, cmap=cm.viridis, rstride=2, cstride=2)
+ax.set_xlabel('$x$')
+ax.set_ylabel('$y$');
